c84e4aba8ee85ed8a8d65ff7f2299cc8d6beefda
best <jaapqu@gmail.com>
2016-07-03 19:48:49 +0800

    add error

--- api/wx_express.php
+++ api/wx_express.php
@@ -13,18 +13,20 @@ function wx_express($action, $id = Null)
     $data = $app->params;
     switch($action){
         case 'express_price_get':
-            //$sending_province_id=get_value($data,'sending_province_id');//发货人省份
             $province=get_value($data,'province');//收货省份
+            if(!$province){
+                error(300,'请填写收货地址');
+            }
             $express_id =get_value($data,'express_id');//快递公司id
             $good_id=get_value($data,'good_id');//货物id
             $good_count=get_value($data,'good_count');//数量
-            //$weight=get_value($data,'weight');//箱重
             $weight=$app->db->get('o_company_goods',['weight'],['gid'=>$good_id]);//查询货物箱重
-
+            if(!$weight){
+                error(300,'无法获取箱重');
+            }
             //查询快递价格
-//            $express_price=$app->db2->select('db_province_express',['first_price','continue_price'],['AND'=>['province'=>$province,'express_id'=>$express_id,'status'=>1]]);
             $express_price=$app->db2->get('db_province_express',['first_price','continue_price'],['AND' =>['province'=>$province,'express_id'=>$express_id,'status'=>1]]);
-            //var_dump($app->db2->last_query());
+
             $data=0;
             if($express_price){
                 $first_price=$express_price['first_price'];
@@ -32,6 +34,8 @@ function wx_express($action, $id = Null)
                 $continue_weight=(int)$weight * (int)$good_count - 1;//续重重量 箱重＊数量－首重1kg
                 $price=$first_price + $continue_weight * $continue_price;//计算运费 首重价格＋(续重重量＊续重价格)
                 $data=$price;
+            }else{
+                error(300,'无法获取运费');
             }
             respCustomer($data);
             break;
