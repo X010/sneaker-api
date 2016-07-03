48ea6d7c4de10a658543947cef52340d73b17481
best <jaapqu@gmail.com>
2016-07-03 16:54:36 +0800

    commit express_price_get

--- api/wx_customer.php
+++ api/wx_customer.php
@@ -534,7 +534,7 @@ function wx_customer($action, $id = Null)
                     unset($unit_good);
                 }
 
-                //买赠 
+                //买赠
                 $goods_arr[$k]['market'] = [];
                 if ($good['marketid']) {
                     $mids = explode(',', $good['marketid']);
@@ -625,7 +625,7 @@ function wx_customer($action, $id = Null)
                     unset($goods_arr[$k]['main_good']);
                 }
 
-                //买赠 
+                //买赠
                 $goods_arr[$k]['market'] = [];
                 if ($good['marketid']) {
                     $mids = explode(',', $good['marketid']);
@@ -696,7 +696,7 @@ function wx_customer($action, $id = Null)
                     }
                     $good['total'] = $good_req[$good['id']];
 
-                    //买赠 
+                    //买赠
                     $good['market'] = [];
                     if ($good['marketid']) {
                         $mids = explode(',', $good['marketid']);
@@ -1447,6 +1447,25 @@ function wx_customer($action, $id = Null)
             // echo $app->db2->last_query();
             // exit;
             respCustomer($data, $total);
+            break;
+        case 'express_price_get':
+            //$sending_province_id=get_value($data,'sending_province_id');//发货人省份
+            $province=get_value($data,'province');//收货省份
+            $express_id =get_value($data,'express_id');//快递公司id
+            $good_id=get_value($data,'good_id');//货物id
+            $good_count=get_value($data,'good_count');//数量
+            //$weight=get_value($data,'weight');//箱重
+
+            $weight=$app->app->db->get('o_company_goods',['weight'],['gid'=>$good_id]);//查询货物箱重
+            //查询快递价格
+            //$express_price=$app->db2->select('db_province_express',['AND'=>['province'=>$province,'express_id'=>$express_id]]);
+            $express_price=$app->db2->get('db_province_express',['first_price','continue_price'],['province'=>$province,'express_id'=>$express_id]);
+            if($express_price){
+              $first_price=$express_price['first_price'];
+              $continue_price=$express_price['continue_price'];
+              $price=$first_price+(($weight*$good_count-1)*$continue_price);//计算运费 首重价格＋((重量－首重1公斤)＊续重价格)
+            }
+            respCustomer($price);
             break;
         case 'app_version':
             $data = [
