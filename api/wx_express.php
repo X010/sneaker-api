205717b31e2e61c8260e3e8fd061867c29b8f48d
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-07-08 21:44:46 +0800

    res

--- api/wx_express.php
+++ api/wx_express.php
@@ -70,30 +70,40 @@ function wx_express($action, $id = Null)
             }
             respCustomer($res);
             break;
-        case 'express_price_get':
-            $province=get_value($data,'province');//收货省份
-            if(!$province){
-                error(300,'请填写收货地址');
+        case 'express_balance_get':
+            //获取该用户的物流余额
+            $scid = get_value($data, 'scid');
+            $ccid = get_value($data, 'ccid');
+            $res = [];
+            if ($scid && $ccid) {
+                $res = $app->db->select("r_customer", "*", ["AND" => ["cid" => scid, "ccid" => $ccid]]);
             }
-            $express_id =get_value($data,'express_id');//快递公司id
-            $good_id=get_value($data,'good_id');//货物id
-            $good_count=get_value($data,'good_count');//数量
-            $weight=$app->db->get('o_company_goods',['weight'],['gid'=>$good_id]);//查询货物箱重
-            if(!$weight){
-                error(300,'无法获取箱重');
+            respCustomer($res);
+            break;
+        case 'express_price_get':
+            $province = get_value($data, 'province');//收货省份
+            if (!$province) {
+                error(300, '请填写收货地址');
+            }
+            $express_id = get_value($data, 'express_id');//快递公司id
+            $good_id = get_value($data, 'good_id');//货物id
+            $good_count = get_value($data, 'good_count');//数量
+            $weight = $app->db->get('o_company_goods', ['weight'], ['gid' => $good_id]);//查询货物箱重
+            if (!$weight) {
+                error(300, '无法获取箱重');
             }
             //查询快递价格
-            $express_price=$app->db2->get('db_province_express',['first_price','continue_price'],['AND' =>['province'=>$province,'express_id'=>$express_id,'status'=>1]]);
+            $express_price = $app->db2->get('db_province_express', ['first_price', 'continue_price'], ['AND' => ['province' => $province, 'express_id' => $express_id, 'status' => 1]]);
 
-            $data=0;
-            if($express_price){
-                $first_price=$express_price['first_price'];
-                $continue_price=$express_price['continue_price'];
-                $continue_weight=(int)$weight * (int)$good_count - 1;//续重重量 箱重＊数量－首重1kg
-                $price=$first_price + $continue_weight * $continue_price;//计算运费 首重价格＋(续重重量＊续重价格)
-                $data=$price;
-            }else{
-                error(300,'无法获取运费');
+            $data = 0;
+            if ($express_price) {
+                $first_price = $express_price['first_price'];
+                $continue_price = $express_price['continue_price'];
+                $continue_weight = (int)$weight * (int)$good_count - 1;//续重重量 箱重＊数量－首重1kg
+                $price = $first_price + $continue_weight * $continue_price;//计算运费 首重价格＋(续重重量＊续重价格)
+                $data = $price;
+            } else {
+                error(300, '无法获取运费');
             }
             respCustomer($data);
             break;
