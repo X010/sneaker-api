40d6a395da4d508867dac59b6a7dd2a46b54a23d
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-08-02 19:44:22 +0800

    edit wx_coupon

--- api/wx_coupon.php
+++ api/wx_coupon.php
@@ -123,6 +123,7 @@ function wx_coupon($action, $id = Null)
                 $order_item_data = json_decode($order_item, true);
                 if ($coupon_id > 0) {
                     $cards = $app->db2->select("db_coupon_detail", "*", ["AND" => ['company_id' => $scid, 'coupon_id' => $coupon_id, 'card_number' => $card_no]], ['LIMIT' => 1]);
+                    $res=$cards;
                     if ($cards&&in_array('merchandise',$cards)) {
                         $goods_list = $cards['merchandise'];
                         if ($goods_list && $order_item_data) {
